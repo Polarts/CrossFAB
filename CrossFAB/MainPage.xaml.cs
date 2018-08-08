a57e059f9c8c58c570176769dc96f918d75ac52f
David Klein <30803298+PolarBearOC@users.noreply.github.com>
2018-08-08 19:13:31 +0300

    Add files via upload

---
+++ CrossFAB/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CrossFAB
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            FB.OnClickCommand = new Command(async () =>
+            {
+                await DisplayAlert("Share", "Shared on Facebook!", "OK!");
+            });
+            TW.OnClickCommand = new Command(async () =>
+            {
+                await DisplayAlert("Share", "Shared on Twitter!", "OK!");
+            });
+            TB.OnClickCommand = new Command(async () =>
+            {
+                await DisplayAlert("Share", "Shared on Tumblr!", "OK!");
+            });
+        }
+    }
+}
